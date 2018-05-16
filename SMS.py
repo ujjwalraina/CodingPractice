b7a40a0693564ac4f1e6830ede756bcdc5f47584
Ujjwal Raina <ujjwal.1rn13cs109@gmail.com>
2018-05-16 21:51:21 +0530

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
-accountSID = 'AC5ed12629f0952973d7d80f06e18b77ff'
-authToken = 'a3f1d7bc46376921f7afa6abb128f60d'
+accountSID = '**************************'
+authToken = '******************************'
 twilioCli = Client(accountSID,authToken)
-myCellPhone = '+12563054104'
-sendNumber = '+917696230555'
+myCellPhone = '+**********'
+sendNumber = '+91**********'
 message = twilioCli.messages.create(body="SMS done through python",from_=myCellPhone,to=sendNumber)
