771cf793dac35d3f0743f441fb8e1d88005700fc
Ujjwal Raina <ujjwal.1rn13cs109@gmail.com>
2018-05-17 18:45:08 +0530

    Delete SMS.py

--- SMS.py
+++
@@ -1,7 +0,0 @@
-from twilio.rest import Client
-accountSID = '**************************'
-authToken = '******************************'
-twilioCli = Client(accountSID,authToken)
-myCellPhone = '+**********'
-sendNumber = '+91**********'
-message = twilioCli.messages.create(body="SMS done through python",from_=myCellPhone,to=sendNumber)
